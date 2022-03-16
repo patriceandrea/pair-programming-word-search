a484f9194ddaeea482d49de50eaa41e6f738f897
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-16 23:22:29 +0000

    completes with pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,8 +3,8 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+describe("#wordSearch()", function () {
+  it("should return false if the word FRANK is not present", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word SEINFELD is present horizontally", function () {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,148 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word LARRY is present vertically", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word VET is present backwards horizontally", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'VET');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word WEST is present backwards vertically", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEST');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word WIFE is present diagonally down-right", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WIFE');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word DAVY is present diagonally down-left", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'GREA');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word SEUL is present diagonally up-right", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEUL');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word UKWRH is present diagonally up-left", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'UKWRH');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the puzzle is empty", function () {
+    const result = wordSearch([], 'TEST');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is empty", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word and puzzle are empty", function () {
+    const result = wordSearch([], '');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,99 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    // Check horizontally
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
+        if (l.split("").reverse().join("").includes(word)) return true
     }
+
+    const y = letters.length;
+    const x = letters[0] ? letters[0].length : 0;
+
+    // Check vertically
+    for (let i = 0; i < x; i++) {
+        let string = "";
+        for (let j = 0; j < y; j++) {
+            string += letters[j][i];
+        }
+        if (string.includes(word)) return true
+        if (string.split("").reverse().join("").includes(word)) return true
+    }
+
+    // Check diagonally up-right/down-left
+    // This loop checks the longest diagonal and above
+    for (let i = y - 1; i >= 0; i--) {
+        let string = "";  // init empty string to push to
+        for (let j = 0; j <= x - 1; j++) {
+            // Check if coordinates in bound; if yes, append to string
+            let iCheck = i - j;
+            let jCheck = j;
+            if ((iCheck) < y && (iCheck) >= 0 && jCheck < x && jCheck >= 0) {
+                string += letters[iCheck][jCheck];
+            }
+        }
+        if (string.includes(word)) return true
+        if (string.split("").reverse().join("").includes(word)) return true
+    }
+    // This loop checks the longest diagonal and below
+    for (let i = 0; i < y - 1; i++) {
+        let string = "";  // init empty string to push to
+        for (let j = 0; j < x; j++) {
+            let iCheck = y - 1 - j + i;
+            let jCheck = j;
+            if ((iCheck) < y && (iCheck) >= 0 && jCheck < x && jCheck >= 0) {
+                string += letters[iCheck][jCheck];
+            }
+        }
+        if (string.includes(word)) return true
+        if (string.split("").reverse().join("").includes(word)) return true
+    }
+
+    // Check diagonally up-left/down-right
+    // This loop checks the longest diagonal and above
+    for (let i = y - 1; i >= 0; i--) {
+        let string = "";  // init empty string to push to
+        for (let j = 0; j <= x - 1; j++) {
+            let iCheck = i - j;
+            let jCheck = x - 1 - j;
+            // Check if coordinates in bound; if yes, append to string
+            if ((iCheck) < y && (iCheck) >= 0 && jCheck < x && jCheck >= 0) {
+                string += letters[iCheck][jCheck];
+            }
+        }
+        if (string.includes(word)) return true
+        if (string.split("").reverse().join("").includes(word)) return true
+    }
+    // This loop checks the longest diagonal and below
+    for (let i = 0; i < y - 1; i++) {
+        let string = "";  // init empty string to push to
+        for (let j = 0; j < x; j++) {
+            let iCheck = y - 1 - j + i;
+            let jCheck = x - 1 - j;
+            // Check if coordinates in bound; if yes, append to string
+            if ((iCheck) < y && (iCheck) >= 0 && jCheck < x && jCheck >= 0) {
+                string += letters[iCheck][jCheck];
+            }
+        }
+        if (string.includes(word)) return true
+        if (string.split("").reverse().join("").includes(word)) return true
+    }
+
+    // Return false if all checks failed
+    return false;
 }
 
 module.exports = wordSearch
+
+/*
+const result = wordSearch([
+  ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'SEINFELD')
+*/
